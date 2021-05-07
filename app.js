5557e09a4f9e347d9d669905f5b8017a2aa31d2c
PraveenKishore <praveenkishore.ec@gmail.com>
2021-05-07 16:42:37 +0530

    changes

--- app.js
+++ app.js
@@ -54,3 +54,7 @@ mongoose.connect('mongodb://localhost/users')
   
 
 module.exports = app;
+
+server.listen(port,()=>{  // do not add localhost here if you are deploying it
+  console.log("server listening to port "+port);
+});
